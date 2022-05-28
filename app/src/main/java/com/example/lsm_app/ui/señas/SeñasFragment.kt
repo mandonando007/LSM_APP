32594ae8d01e17bedda2f3043837a8cb8ef1dcd5
ArtBaMa22 <arturobama17@hotmail.com>
2022-05-27 22:08:21 -0500

    27-05-2022, 10:07pm
    fragment señas

--- app/src/main/java/com/example/lsm_app/ui/señas/SeñasFragment.kt
+++ app/src/main/java/com/example/lsm_app/ui/señas/SeñasFragment.kt
@@ -17,37 +17,73 @@ import com.example.lsm_app.R
 import androidx.cardview.widget.CardView
 import com.example.lsm_app.Senas
 import com.example.lsm_app.databinding.FragmentSenasBinding
+import com.example.lsm_app.nav_letras
 
 class SeñasFragment : Fragment(R.layout.fragment_senas) {
     var imagen: ImageView? = null
     var miview: View? = null
-    var btn: CardView? = null
+    var btnFrutas: CardView? = null
+    var btnVerduras: CardView? = null
+    var btnAlimentos: CardView? = null
+    var btnColores: CardView? = null
+    var btnDias: CardView? = null
+    var btnAnimales: CardView? = null
+    var btnNumeros: CardView? = null
+    var btnMeses: CardView? = null
+    var btnVerbos: CardView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
          val vista = inflater.inflate(R.layout.fragment_senas, container, false)
         miview = vista
-        imagen = miview?.findViewById(R.id.imgFrutas)
-     //   imagen?.setImageResource(R.drawable.cerrar_sesion)
 
-        miview = vista
-        btn = miview?.findViewById(R.id.cardfrutas)
-        btn?.setOnClickListener{
+        btnFrutas = miview?.findViewById(R.id.cardfrutas)
+        btnVerduras = miview?.findViewById(R.id.verduras)
+        btnAlimentos = miview?.findViewById(R.id.alimentos)
+        btnColores = miview?.findViewById(R.id.colores)
+        btnDias = miview?.findViewById(R.id.cardDias)
+        btnAnimales = miview?.findViewById(R.id.animales)
+        btnNumeros = miview?.findViewById(R.id.numeros)
+        btnMeses = miview?.findViewById(R.id.meses)
+        btnVerbos = miview?.findViewById(R.id.Verbos)
 
-        }
         return vista
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-
+        btnFrutas?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnVerduras?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnAlimentos?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnColores?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnDias?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnNumeros?.setOnClickListener{
+            setCurrentFragment(Senas())
+        }
+        btnVerbos?.setOnClickListener {
+            setCurrentFragment(Senas())
+        }
 
 
 
     }
 
-
+    private fun setCurrentFragment(fragment: Fragment ){
+        var fr = getFragmentManager()?.beginTransaction()
+        fr?.replace(R.id.nav_host_fragment_activity_main, fragment)
+        fr?.addToBackStack(null)
+        fr?.commit()
+    }
 }
