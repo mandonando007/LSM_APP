a75cb5a5599f9120f5993923e56c3664a7544fe2
mandonando007 <jesusarmando51@gmail.com>
2022-05-29 16:29:00 -0500

    videos Terminado/IconoApp

--- app/src/main/java/com/example/lsm_app/ui/videos/VideosFragment.kt
+++ app/src/main/java/com/example/lsm_app/ui/videos/VideosFragment.kt
@@ -14,9 +14,22 @@ import com.example.lsm_app.videosView
 class VideosFragment : Fragment(R.layout.fragment_videos) {
 
     var miview: View? = null
+    var btnEstaciones: CardView? = null
+    var btnColores: CardView? = null
+    var btnVerbos: CardView? = null
+    var btnPreguntas: CardView? = null
+    var btnTiempo: CardView? = null
+    var btnEscuela: CardView? = null
+    var btnFamilia: CardView? = null
+    var btnDias: CardView? = null
+    var btnNumeros: CardView? = null
+    var btnMeses: CardView? = null
     var btnSaludos: CardView? = null
+    var btnAlfabeto: CardView? = null
 
     var URL : String  = "";
+    var nomVideo : String = ""
+    var nomCom: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +39,18 @@ class VideosFragment : Fragment(R.layout.fragment_videos) {
         val vista = inflater.inflate(R.layout.fragment_videos, container, false)
         miview = vista
         btnSaludos = miview?.findViewById(R.id.cardSaludos)
+        btnAlfabeto = miview?.findViewById(R.id.cardAlfabeto)
+        btnDias = miview?.findViewById(R.id.cardDias)
+        btnEstaciones = miview?.findViewById(R.id.cardEstaciones)
+        btnFamilia = miview?.findViewById(R.id.cardFamilia)
+        btnEscuela = miview?.findViewById(R.id.cardEscuela)
+        btnMeses = miview?.findViewById(R.id.cardMeses)
+        btnNumeros = miview?.findViewById(R.id.cardNumeros)
+        btnPreguntas = miview?.findViewById(R.id.cardPreguntas)
+        btnTiempo = miview?.findViewById(R.id.cardTiempo)
+        btnColores = miview?.findViewById(R.id.cardColores)
+        btnVerbos = miview?.findViewById(R.id.cardVerbos)
+
 
         return vista
     }
@@ -36,19 +61,112 @@ class VideosFragment : Fragment(R.layout.fragment_videos) {
         super.onViewCreated(view, savedInstanceState)
 
         btnSaludos?.setOnClickListener{
-            URL = "https://drive.google.com/file/d/1oqdMWr4pK-1ZWjHrJz7xWItJQ5Hi7qiC/view?usp=sharing"
+            nomVideo = "saludos"
+            nomCom = "saludos.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/saludos/"
             val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
-            setCurrentFragment(URL)
+            setCurrentFragment(URL, nomVideo, nomCom)
         }
+
+        btnAlfabeto?.setOnClickListener{
+            nomVideo = "alfabeto"
+            nomCom = "alfabeto.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/alfabeto/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnDias?.setOnClickListener{
+            nomVideo = "dias"
+            nomCom = "dias.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/dias/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnEstaciones?.setOnClickListener{
+            nomVideo = "estaciones"
+            nomCom = "estaciones.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/estaciones/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnFamilia?.setOnClickListener{
+            nomVideo = "familia"
+            nomCom = "familia.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/familia/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnEscuela?.setOnClickListener{
+            nomVideo = "escuela"
+            nomCom = "escuela.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/escuela/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnMeses?.setOnClickListener{
+            nomVideo = "meses"
+            nomCom = "meses.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/meses/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnNumeros?.setOnClickListener{
+            nomVideo = "numeros"
+            nomCom = "numeros.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/numeros/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnPreguntas?.setOnClickListener{
+            nomVideo = "preguntas"
+            nomCom = "preguntas.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/preguntas/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnTiempo?.setOnClickListener{
+            nomVideo = "tiempo"
+            nomCom = "tiempo.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/tiempo/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnColores?.setOnClickListener{
+            nomVideo = "colores"
+            nomCom = "colores.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/colores/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
+        btnVerbos?.setOnClickListener{
+            nomVideo = "verbos"
+            nomCom = "verbos.mp4"
+            URL = "gs://lsm-app-8708e.appspot.com/verbos/"
+            val toast = Toast.makeText(this.context ,"Funciona el boton", Toast.LENGTH_SHORT).show()
+            setCurrentFragment(URL, nomVideo, nomCom)
+        }
+
 
 
 
     }
 
-    private fun setCurrentFragment(url: String) {
+    private fun setCurrentFragment(url: String,nomVid: String, nomCom: String) {
 
         val bundle = Bundle()
         bundle.putString("key", url)
+        bundle.putString("key1", nomVid)
+        bundle.putString("key2", nomCom)
         val fragment = videosView()
         fragment.arguments = bundle
 
@@ -57,5 +175,7 @@ class VideosFragment : Fragment(R.layout.fragment_videos) {
         fr?.addToBackStack(null)
         fr?.commit()
     }
+
+
 
 }
--- app/src/main/java/com/example/lsm_app/videosView.kt
+++ app/src/main/java/com/example/lsm_app/videosView.kt
@@ -8,12 +8,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FileDownloadTask
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.io.IOException
 
 
 class videosView : Fragment(R.layout.fragment_videos_view) {
 
     var URL: String = ""
+    var nomVid: String = ""
+    var nomCom: String = ""
+    var videoViw: VideoView?= null
+    var miVista : View? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,12 +33,9 @@ class videosView : Fragment(R.layout.fragment_videos_view) {
     ): View? {
         // Inflate the layout for this fragment
         val video = inflater.inflate(R.layout.fragment_videos_view, container, false)
-
-               val vid = video.findViewById(R.id.videoView) as VideoView
-
-        vid.setVideoURI(Uri.parse(URL))
-        vid.setMediaController(MediaController(requireActivity()))
-        vid.start()
+        miVista = video
+        videoViw = miVista?.findViewById(R.id.videoView)
+        obtenerVideo(nomVid,nomCom)
 
         return video
     }
@@ -34,9 +43,26 @@ class videosView : Fragment(R.layout.fragment_videos_view) {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if(arguments != null){
-             URL = arguments?.getString("key").toString()
+            URL = arguments?.getString("key").toString()
+            nomVid = arguments?.getString("key1").toString()
+            nomCom = arguments?.getString("key2").toString()
         }
 
+    }
+
+    private fun obtenerVideo(nomVid: String, nomCom:String){
+        var storage = FirebaseStorage.getInstance()
+        var storageRef = storage.getReferenceFromUrl(URL).child(nomCom)
+        try {
+            val localFile: File = File.createTempFile(nomVid, "mp4")
+            storageRef.getFile(localFile)
+                .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot?> {
+                    videoViw?.setVideoURI(Uri.fromFile(localFile))
+                    videoViw?.start()
+                }).addOnFailureListener(OnFailureListener { })
+        } catch (e: IOException) {
+            Toast.makeText(context, "Error al Cargar el Video", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
